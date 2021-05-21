dc71ef5a183ed667fd3f92295f4255f8eac890fa
Lee, SoRa <8282445@naver.com>
2021-05-21 17:31:34 +0900

    SR Shop Opening commit

---
+++ src/com/srlee/shop/IceCreamType.java
@@ -0,0 +1,5 @@
+package com.srlee.shop;
+
+public enum IceCreamType {
+	컵, 바, 콘
+}
---
+++ src/com/srlee/shop/Shop.java
@@ -0,0 +1,11 @@
+package com.srlee.shop;
+
+public class Shop {
+
+	public static void main(String[] args) {
+		IceCreamType ic = IceCreamType.바;
+		System.out.println("SR 다방에 환영합니다");
+		System.out.println(IceCreamType.컵);		
+	}
+
+}
